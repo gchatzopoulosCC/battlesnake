6758a9a5f5def6b72a0adfd9ebc72eb2e9290a83
George Chatzopoulos <40667560-gchatzopoulos1@users.noreply.replit.com>
2025-03-29 18:15:25 +0000

    Rewrite avoidWalls functions for better clarity

--- src/common/move.js
+++ src/common/move.js
@@ -1,13 +1,13 @@
 // Common functions for the move method
 function avoidWalls(gameState, isMoveSafe) {
-  const boardWidth = gameState.board.width;
-  const boardHeight = gameState.board.height;
-  const myHead = gameState.you.body[0];
+  const { width: boardWidth, height: boardHeight } = gameState.board;
+  const { x, y } = gameState.you.body[0];
 
-  if (myHead.x === 0) isMoveSafe.left = false; // Left wall
-  if (myHead.x === boardWidth - 1) isMoveSafe.right = false; // Right wall
-  if (myHead.y === 0) isMoveSafe.down = false; // Bottom wall
-  if (myHead.y === boardHeight - 1) isMoveSafe.up = false; // Top wall
+  // Check wall collisions
+  if (x === 0) isMoveSafe.left = false;
+  if (x === boardWidth - 1) isMoveSafe.right = false;
+  if (y === 0) isMoveSafe.down = false;
+  if (y === boardHeight - 1) isMoveSafe.up = false;
 
   return isMoveSafe;
 }
