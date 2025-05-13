27ee86571760c90d8693f16a115ac47f1acbaba3
george-chatzopoulos <georgechatzopoulos2805@gmai.com>
2025-05-13 18:24:42 +0300

    refactor(avoidanceChecker): Create a createAvoidanceChecker to check for available moves to replace the manual check inside avoidSelf and avoidOthers

---
+++ src/utils/moves/avoidanceChecker.js
@@ -0,0 +1,14 @@
+import { getMoves } from "../../common/sets/moves.js";
+
+export function createAvoidanceChecker(getMapFunction) {
+  return function checkAvoidance(gameState, isMoveSafe) {
+    const map = getMapFunction(gameState);
+    const moves = getMoves(gameState);
+
+    for (const move of moves) {
+      if (map.has(move.getAdjacentPosition)) {
+        isMoveSafe[move.direction] = false;
+      }
+    }
+  };
+}
