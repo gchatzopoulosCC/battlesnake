92d9414a13ce6dda459e70836c06775c9f60aa10
george-chatzopoulos <georgechatzopoulos2805@gmai.com>
2025-05-13 18:42:37 +0300

    refactor(avoidSelf): Transfer the avoidanceChecker variable declaration outside of the function

--- src/lib/moves/avoidSelf.js
+++ src/lib/moves/avoidSelf.js
@@ -1,8 +1,9 @@
 import { getBodyMap } from "../bodyMap/bodyMap.js";
 import { createAvoidanceChecker } from "../../utils/moves/avoidanceChecker.js";
 
+const avoidanceChecker = createAvoidanceChecker(getBodyMap);
+
 export function avoidSelf(gameState, isMoveSafe) {
-  const avoidanceChecker = createAvoidanceChecker(getBodyMap);
   avoidanceChecker(gameState, isMoveSafe);
 
   return isMoveSafe;
