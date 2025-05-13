7daf7e616bfa70671eeb62e52b914c8ccfc76784
george-chatzopoulos <georgechatzopoulos2805@gmai.com>
2025-05-13 18:30:16 +0300

    refactor(avoidOthers): rename the variable checkAvoidance to avoidanceChecker

--- src/lib/moves/avoidOthers.js
+++ src/lib/moves/avoidOthers.js
@@ -2,8 +2,8 @@ import { getcollisionMap } from "../collisionMap/collisionMap.js";
 import { createAvoidanceChecker } from "../../utils/moves/avoidanceChecker.js";
 
 export function avoidOthers(gameState, isMoveSafe) {
-  const checkAvoidance = createAvoidanceChecker(getcollisionMap);
-  checkAvoidance(gameState, isMoveSafe);
+  const avoidanceChecker = createAvoidanceChecker(getcollisionMap);
+  avoidanceChecker(gameState, isMoveSafe);
 
   return isMoveSafe;
 }
