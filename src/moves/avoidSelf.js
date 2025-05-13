d40e767b43f40060525823589f4ddad6ccfb9fd2
george-chatzopoulos <georgechatzopoulos2805@gmai.com>
2025-05-13 17:31:44 +0300

    refactor(avoidSelf): Use the common functions instead of locally defined operations

--- src/moves/avoidSelf.js
+++ src/moves/avoidSelf.js
@@ -1,17 +1,15 @@
-import { getMoves } from "../lib/collisionSet/moves.js";
-import * as pos from "../lib/collisionSet/adjacentPositions.js";
+import { getMoves } from "../common/sets/moves.js";
 import { getBodySet } from "../lib/bodySet/bodySet.js";
 
 export function avoidSelf(gameState, isMoveSafe) {
-  const { x: headX, y: headY } = gameState.you.body[0];
-
-  // Create a set of body positions (excluding head)
   const bodySet = getBodySet(gameState);
+  const moves = getMoves(gameState);
 
-  if (bodySet.has(`${headX - 1},${headY}`)) isMoveSafe.left = false;
-  if (bodySet.has(`${headX + 1},${headY}`)) isMoveSafe.right = false;
-  if (bodySet.has(`${headX},${headY - 1}`)) isMoveSafe.down = false;
-  if (bodySet.has(`${headX},${headY + 1}`)) isMoveSafe.up = false;
+  for (const move of moves) {
+    if (bodySet.has(move.getAdjacentPosition)) {
+      isMoveSafe[move.direction] = false;
+    }
+  }
 
   return isMoveSafe;
 }
