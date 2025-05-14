50882c5dedf3478dbf0b7960418fb69731d21b64
george-chatzopoulos <georgechatzopoulos2805@gmai.com>
2025-05-14 11:04:27 +0300

    refactor(collisionSet): Simplify the content acquisition of the collisionSet

--- src/lib/sets/collisionSet.js
+++ src/lib/sets/collisionSet.js
@@ -1,14 +1,11 @@
 export function getCollisionSet(gameState) {
-  const collisionSet = new Set();
   const otherSnakes = gameState.board.snakes.filter(
     (snake) => snake.id !== gameState.you.id,
   );
 
-  otherSnakes.forEach((snake) => {
-    snake.body.forEach((segment) => {
-      collisionSet.add(`${segment.x},${segment.y}`);
-    });
-  });
+  const collisionCoordinates = otherSnakes
+    .flatMap((snake) => snake.body)
+    .map((segment) => `${segment.x},${segment.y}`);
 
-  return collisionSet;
+  return new Set(collisionCoordinates);
 }
