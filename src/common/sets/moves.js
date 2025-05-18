8bb4b46ce07c2076e21bdbb04244c1cfa6bca838
george <gchatzopoulos@york.citycollege.eu>
2025-05-18 23:33:21 +0300

    refactor(moves): Use getHead(gameState) instead of retrieving the head of the snake manually

--- src/common/sets/moves.js
+++ src/common/sets/moves.js
@@ -1,7 +1,8 @@
 import * as pos from "./adjacentPositions.js";
+import { getHead } from "../snake/body.js";
 
 export function getMoves(gameState) {
-  const head = gameState.you.body[0];
+  const head = getHead(gameState);
   const moves = [
     {
       direction: "left",
