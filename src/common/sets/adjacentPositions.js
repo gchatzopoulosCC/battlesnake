c42b6c802b50fd46bbff774ae61cbaaf413af9bb
george <gchatzopoulos@york.citycollege.eu>
2025-05-19 20:48:41 +0300

    fix(adjacentPositions): Replace the return type of each function from Object to String

--- src/common/sets/adjacentPositions.js
+++ src/common/sets/adjacentPositions.js
@@ -11,13 +11,13 @@ import { parseCoordinates } from "./coordinates.js";
 /**
  * @description This function returns the left adjacent position of a given position.
  * It subtracts 1 from the x-coordinate of the position.
- * @param {Object} position - The current position of the snake's head.
+ * @param {Object} position - The current position on the board.
  * @param {number} position.x - The x-coordinate of the position.
  * @param {number} position.y - The y-coordinate of the position.
- * @returns {Object} - The left adjacent position, represented as an object with `x` and `y` coordinates.
+ * @returns {string} - The left adjacent position in string format ("x,y").
  * @example
  * const position = { x: 5, y: 5 };
- * getLeftAdjacentPosition(position); // Returns { x: 4, y: 5 }
+ * getLeftAdjacentPosition(position); // Returns "4,5"
  */
 function getLeftAdjacentPosition(position) {
   return parseCoordinates(position.x - 1, position.y);
@@ -26,14 +26,14 @@ function getLeftAdjacentPosition(position) {
 
 /**
  * @description This function returns the right adjacent position of a given position.
- * It subtracts 1 from the x-coordinate of the position.
- * @param {Object} position - The current position of the snake's head.
+ * It adds 1 to the x-coordinate of the position.
+ * @param {Object} position - The current position on the board.
  * @param {number} position.x - The x-coordinate of the position.
  * @param {number} position.y - The y-coordinate of the position.
- * @returns {Object} - The right adjacent position, represented as an object with `x` and `y` coordinates.
+ * @returns {string} - The right adjacent position in string format ("x,y").
  * @example
  * const position = { x: 5, y: 5 };
- * getRightAdjacentPosition(position); // Returns { x: 6, y: 5 }
+ * getRightAdjacentPosition(position); // Returns "6,5"
  */
 function getRightAdjacentPosition(position) {
   return parseCoordinates(position.x + 1, position.y);
@@ -42,14 +42,14 @@ function getRightAdjacentPosition(position) {
 
 /**
  * @description This function returns the up adjacent position of a given position.
- * It subtracts 1 from the x-coordinate of the position.
- * @param {Object} position - The current position of the snake's head.
+ * It adds 1 to the y-coordinate of the position.
+ * @param {Object} position - The current position on the board.
  * @param {number} position.x - The x-coordinate of the position.
  * @param {number} position.y - The y-coordinate of the position.
- * @returns {Object} - The up adjacent position, represented as an object with `x` and `y` coordinates.
+ * @returns {string} - The up adjacent position in string format ("x,y").
  * @example
  * const position = { x: 5, y: 5 };
- * getUpAdjacentPosition(position); // Returns { x: 5, y: 6 }
+ * getUpAdjacentPosition(position); // Returns "5,6"
  */
 function getUpAdjacentPosition(position) {
   return parseCoordinates(position.x, position.y + 1);
@@ -58,14 +58,14 @@ function getUpAdjacentPosition(position) {
 
 /**
  * @description This function returns the down adjacent position of a given position.
- * It subtracts 1 from the x-coordinate of the position.
- * @param {Object} position - The current position of the snake's head.
+ * It subtracts 1 from the y-coordinate of the position.
+ * @param {Object} position - The current position on the board.
  * @param {number} position.x - The x-coordinate of the position.
  * @param {number} position.y - The y-coordinate of the position.
- * @returns {Object} - The down adjacent position, represented as an object with `x` and `y` coordinates.
+ * @returns {string} - The down adjacent position in string format ("x,y").
  * @example
  * const position = { x: 5, y: 5 };
- * getDownAdjacentPosition(position); // Returns { x: 5, y: 4 }
+ * getDownAdjacentPosition(position); // Returns "5,4"
  */
 function getDownAdjacentPosition(position) {
   return parseCoordinates(position.x, position.y - 1);
