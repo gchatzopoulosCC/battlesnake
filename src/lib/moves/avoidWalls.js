8fef29b6958f575b08cab80e77a04a7b7db18288
george <gchatzopoulos@york.citycollege.eu>
2025-05-19 20:24:43 +0300

    docs(avoidWalls): Write appropriate documentation for all the contents of the file

--- src/lib/moves/avoidWalls.js
+++ src/lib/moves/avoidWalls.js
@@ -1,3 +1,43 @@
+/**
+ * @file avoidWalls.js
+ * @description This module contains a function to check if the snake is at the 
+ * edge of the board and update the isMoveSafe object accordingly.
+ * @module src/lib/moves/avoidWalls
+ */
+
+/**
+ * @description This function checks if the snake is at the edge of the board
+ * and updates the isMoveSafe object accordingly.
+ * @param {Object} gameState - The current state of the game.
+ * @param {Object} isMoveSafe - An object containing the possible moves and their safety status.
+ * @param {boolean} isMoveSafe.up - Indicates if moving up is safe.
+ * @param {boolean} isMoveSafe.down - Indicates if moving down is safe.
+ * @param {boolean} isMoveSafe.left - Indicates if moving left is safe.
+ * @param {boolean} isMoveSafe.right - Indicates if moving right is safe.
+ * @returns {Object} The updated isMoveSafe object.
+ * @example
+ * const gameState = {
+ *  board: {
+ *   width: 11,
+ *   height: 11,
+ *  },
+ *  you: {
+ *    body: [
+ *      { x: 5, y: 5 },
+ *      { x: 5, y: 4 },
+ *      { x: 5, y: 3 },
+ *    ],
+ *  },
+ * };
+ * const isMoveSafe = {
+ *  left: true,
+ *  right: true,
+ *  up: true,
+ *  down: true,
+ * };
+ * 
+ * avoidWalls(gameState, isMoveSafe);
+ */
 export function avoidWalls(gameState, isMoveSafe) {
   const { width: boardWidth, height: boardHeight } = gameState.board;
   const { x, y } = gameState.you.body[0];
