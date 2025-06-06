8f1c7cd88724a675e6977b000f5682ceb6ffea1c
george-chatzopoulos <georgechatzopoulos2805@gmai.com>
2025-06-06 13:39:26 +0300

    fix: Change the coverage collection from src/**/*.js to tests/**/*.js

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,14 @@
 export default {
   // Specify the environment the tests will run in
   testEnvironment: 'node',
-  
+
   // Use of ESM as support
   transform: {},
-  
-  // Applying coverage for the project 
+
+  // Applying coverage for the project
   collectCoverage: true,
   collectCoverageFrom: [
-    'src/**/*.js',
+    'tests/**/*.js',
     '!**/*.test.js',
     '!**/node_modules/**',
     '!jest.config.js',
@@ -16,10 +16,10 @@ export default {
     '!server.js',
     '!index.js'
   ],
-  
+
   // The directory where Jest should output its coverage files after running the tests
   coverageDirectory: 'coverage',
-  
+
   // A threshold that Jest will use to fail tests if at least 50% of the branches, functions, lines, and statements are not covered
   coverageThreshold: {
     global: {
@@ -29,16 +29,16 @@ export default {
       statements: 50
     }
   },
-  
+
   // Formatting coverage for the output
   coverageReporters: ['text', 'html'],
-  
+
   // Display tests with proper formatting
   verbose: true,
-  
+
   // Applying test matching pattern for easier and more specific testing
   testMatch: ['**/__tests__/**/*.js', '**/*.test.js'],
-  
-  // Timeout for tests 
+
+  // Timeout for tests
   testTimeout: 1000
-}; 
+};
