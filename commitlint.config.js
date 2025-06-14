1d973c36e51c879fb9aa48cf068d8d6029b4d730
george <gchatzopoulos@york.citycollege.eu>
2025-06-14 15:53:01 +0300

    feat(commitlint.config): Install and configure commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "docs", "style", "refactor", "test", "chore", "revert"],
+    ],
+    "subject-case": [2, "always", ["sentence-case", "start-case", "pascal-case"]],
+    "scope-case": [2, "always", "lower-case"],
+    "body-max-line-length": [2, "always", 72],
+  },
+};
